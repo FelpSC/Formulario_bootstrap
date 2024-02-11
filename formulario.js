5bfe10a5008aa6b9721ecda025ea9b13a42c5aad
FelpSC <felipetoral1998@gmail.com>
2024-02-11 19:38:00 -0300

    js de data/hora

--- formulario.js
+++ formulario.js
@@ -1,4 +1,21 @@
-const inputfile = 
-document.querySelector("#inputimg");
-const pictureImage = 
-document.querySelector("")
+document.addEventListener("DOMContentLoaded", function() {
+                        
+    const inputs = document.querySelectorAll('#data');
+
+    inputs.forEach(function(input) {
+        input.addEventListener('input', function(event) {
+            let value = event.target.value.replace(/\D/g, '').substring(0, 8);
+
+            if (value.length > 4) {
+                value = value.replace(/(\d{2})(\d{2})(\d{4})/, '$1/$2/$3');
+
+            } else if (value.length > 2){
+                value = value.replace(/(\d{2})(\d{2})/, '$1/$2');
+
+            }
+
+            event.target.value = value;
+
+        });
+    });
+});
